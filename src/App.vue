b0d60c6b8ac1b7c71e0c108f12963f4a2e415172
Anas Mammeri <anas.mammeri@gmail.com>
2016-06-25 15:16:11 +0200

    feat: basic sign in view.

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,29 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+  <div class="app-content">
+    <div class="row logo">
+      <div class="col s12">
+        <i class="large material-icons light-blue-text text-darken-4">person_pin</i>
+      </div>
+    </div>
+    <sign-in></sign-in>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import SignIn from './components/SignIn/SignIn'
 
 export default {
   components: {
-    Hello
+    SignIn
   }
 }
 </script>
 
-<style>
-html {
-  height: 100%;
-}
+<style lang="less">
+@color: #2c3e50;
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
+.app-content {
+  padding-top: 70px;
+  .logo { text-align: center; }
 }
 </style>
---
+++ src/components/SignIn/SignIn.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row sign-in">
+    <div class="col s12 m10 offset-m1 l6 offset-l3">
+      <form class="card">
+        <div class="card-title">Sign In</div>
+        <div class="card-content">
+          <md-input type="email">Email</md-input>
+          <md-input type="password">Password</md-input>
+          <md-btn large class="waves-effect waves-light light-blue darken-4">Connection</md-btn>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style lang="less">
+  .sign-in {
+    .card { padding: 45px 30px; }
+    .card-content {
+      text-align: center;
+      .input-field { label { left: 0; }}
+      .md-button { margin-top: 45px; }
+    }
+  }
+</style>
