35276fe72b08dd9caab5601fb772212384d9ab07
Anas Mammeri <anas.mammeri@gmail.com>
2016-07-01 20:46:45 +0200

    feat: basic create account vue

--- src/components/SignIn/SignIn.vue
+++ src/components/SignIn/SignIn.vue
@@ -1,16 +1,34 @@
 <template>
   <div class="row sign-in">
     <div class="col s12 m10 offset-m1 l6 offset-l3">
-      <form class="card">
-        <div class="card-title">Sign In</div>
-        <div class="card-content">
-          <md-input type="email">Email</md-input>
-          <md-input type="password">Password</md-input>
-          <div class="reset-password">I don't remember my password</div>
-          <md-btn large class="waves-effect waves-light light-blue darken-4">Connection</md-btn>
-          <div class="no-account">You don't have an account ? You can <a href="">create one</a></div>
-        </div>
-      </form>
+      <div class="card">
+        <form v-show="!createForm">
+          <div class="card-title">Sign In</div>
+          <div class="card-content">
+            <md-input type="email">Email</md-input>
+            <md-input type="password">Password</md-input>
+            <div class="reset-password">I don't remember my password</div>
+            <md-btn large class="waves-effect waves-light light-blue darken-4">Connection</md-btn>
+            <div class="no-account">You don't have an account ? You can <a @click="createForm = true">create one</a></div>
+          </div>
+        </form>
+
+        <form v-show="createForm">
+          <div class="card-title">Create account</div>
+          <div class="go-back">
+            <md-button class="btn waves-effect waves-light light-blue darken-4" @click="createForm = false" floating>
+              <i class="icon-arrow-left"></i>
+            </md-button>
+          </div>
+          <div class="card-content">
+            <md-input type="text">First Name</md-input>
+            <md-input type="text">Last Name</md-input>
+            <md-input type="email">Email</md-input>
+            <md-input type="password">Password</md-input>
+            <md-btn large class="waves-effect waves-light light-blue darken-4">Create</md-btn>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -18,8 +36,15 @@
 <script>
   export default {
     data () {
-      return {}
+      return {
+        createForm: false
+      }
     }
+    // methods: {
+    //   example: function () {
+    //     // code
+    //   }
+    // }
   }
 </script>
 
@@ -50,8 +75,21 @@
       }
     }
     .no-account {
-      font-size: 12px;
-      a { text-decoration: underline; }
+      font-size: 13px;
+      a {
+        text-decoration: underline;
+        &:hover { cursor: pointer; }
+      }
+    }
+    .go-back {
+      position: absolute;
+      right: 30px;
+      top: 30px;
+      i {
+        font-size: 15px;
+        margin-left: -2px;
+        font-weight: bold;
+      }
     }
   }
 </style>
