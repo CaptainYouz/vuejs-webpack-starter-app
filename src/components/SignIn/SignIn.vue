2dbbbe2f8939ae6e5feeeb20f73e8b79a6597c9e
Anas Mammeri <anas.mammeri@gmail.com>
2016-07-03 12:49:58 +0200

    feat: basic reset password view

--- src/components/SignIn/SignIn.vue
+++ src/components/SignIn/SignIn.vue
@@ -2,12 +2,12 @@
   <div class="row sign-in">
     <div class="col s12 m10 offset-m1 l6 offset-l3">
       <div class="card">
-        <form v-show="!createForm">
+        <form v-show="!createForm && !resetPasswordForm">
           <div class="card-title">Sign In</div>
           <div class="card-content">
             <md-input type="email">Email</md-input>
             <md-input type="password">Password</md-input>
-            <div class="reset-password">I don't remember my password</div>
+            <div class="reset-password" @click="resetPasswordForm = true">I don't remember my password</div>
             <md-btn large class="waves-effect waves-light light-blue darken-4">Connection</md-btn>
             <div class="no-account">You don't have an account ? You can <a @click="createForm = true">create one</a></div>
           </div>
@@ -28,6 +28,19 @@
             <md-btn large class="waves-effect waves-light light-blue darken-4">Create</md-btn>
           </div>
         </form>
+
+        <form v-show="resetPasswordForm">
+          <div class="card-title">Reset your password</div>
+          <div class="go-back">
+            <md-button class="btn waves-effect waves-light light-blue darken-4" @click="resetPasswordForm = false" floating>
+              <i class="icon-arrow-left"></i>
+            </md-button>
+          </div>
+          <div class="card-content">
+            <md-input type="text">Email</md-input>
+            <md-btn large class="waves-effect waves-light light-blue darken-4">Send an e-mail</md-btn>
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -37,7 +50,8 @@
   export default {
     data () {
       return {
-        createForm: false
+        createForm: false,
+        resetPasswordForm: false
       }
     }
     // methods: {
